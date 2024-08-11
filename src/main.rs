d79f48608f103a07790a93da71c222d7b84603b2
LelouchFR <baptistezahnow@gmail.com>
2024-08-11 03:53:38 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use std::fs;
+use colored::Colorize;
+
+mod types;
+
+use crate::types::files;
+
+fn main() -> std::io::Result<()> {
+    let path = ".";
+    
+    for entry in fs::read_dir(path)? {
+        let entry = entry?;
+        let metadata = entry.metadata()?;
+        let file_name = entry.file_name();
+        let file_name_str = file_name.to_string_lossy();
+
+        if metadata.is_dir() {
+            println!("{} {}", "".blue(), file_name_str.blue().bold());
+        } else if metadata.is_file() {
+            println!("{}", files::render_file(file_name_str.to_string(), files::get_file_type(file_name_str.to_string())));
+        } else if metadata.is_symlink() {
+            println!("{}", file_name_str.green().bold());
+        }
+    }
+
+    Ok(())
+}
---
+++ src/types/files.rs
@@ -0,0 +1,67 @@
+use std::path::Path;
+use colored::Colorize;
+
+pub enum FileType {
+    Config,
+    Rust,
+    C,
+    CPP,
+    CS,
+    Zig,
+    Python,
+    JavaScript,
+    TypeScript,
+    Html,
+    Css,
+    Scss,
+    React,
+    None,
+}
+
+pub fn get_file_type(file: String) -> FileType {
+    let path = Path::new(&file);
+
+    match path.extension() {
+        Some(ext) => {
+            match ext.to_string_lossy().as_ref() {
+                "rs" => FileType::Rust,
+                "c" => FileType::C,
+                "cpp" | "cc" | "cxx" => FileType::CPP,
+                "cs" => FileType::CS,
+                "zig" => FileType::Zig,
+                "py" => FileType::Python,
+                "js" => FileType::JavaScript,
+                "ts" => FileType::TypeScript,
+                "htm" | "html" => FileType::Html,
+                "css" => FileType::Css,
+                "scss" | "sass" => FileType::Scss,
+                "jsx" | "tsx" => FileType::React,
+                _  => FileType::Config
+            }
+        },
+        None => FileType::None,
+    }
+}
+
+pub fn get_file_type_icon(file_type: FileType) -> String {
+    match file_type {
+        FileType::Rust => format!("{}", "".truecolor(206, 66, 43)),
+        FileType::Config => format!("{}", "".truecolor(128, 128, 128)),
+        FileType::C => format!("{}", "".truecolor(57, 74, 171)),
+        FileType::CPP => format!("{}", "".truecolor(0, 89, 156)),
+        FileType::CS => format!("{}", "󰌛".truecolor(149, 60, 173)),
+        FileType::Zig => format!("{}", "".truecolor(247, 164, 29)),
+        FileType::Python => format!("{}", "".truecolor(255, 224, 82)),
+        FileType::JavaScript => format!("{}", "".truecolor(240, 219, 79)),
+        FileType::TypeScript => format!("{}", "".truecolor(0, 122, 204)),
+        FileType::Html => format!("{}", "".truecolor(225, 78, 29)),
+        FileType::Css => format!("{}", "".truecolor(2, 119, 189)),
+        FileType::Scss => format!("{}", "".truecolor(205, 103, 153)),
+        FileType::React => format!("{}", "".truecolor(0, 216, 255)),
+        _ => "".to_string()
+    }
+}
+
+pub fn render_file(file_name: String, file_type: FileType) -> String {
+    format!("{} {}", get_file_type_icon(file_type), file_name)
+}
