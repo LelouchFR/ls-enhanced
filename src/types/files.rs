fdac55204c279b17983ff06cd23c7932588be438
LelouchFR <baptistezahnow@gmail.com>
2024-08-12 02:20:48 +0200

    added track of .gitattributes files into FileType & refactoring

--- src/types/files.rs
+++ src/types/files.rs
@@ -26,14 +26,16 @@ pub enum FileType {
 pub fn get_file_type(file: String) -> FileType {
     let path = Path::new(&file);
 
+    // for specific files/dotfiles
     if let Some(dotfile) = path.file_name().and_then(|file_name| file_name.to_str()) {
         match dotfile {
-            ".gitignore" | ".gitmodules" => return FileType::Git,
+            ".gitignore" | ".gitmodules" | ".gitattributes" => return FileType::Git,
             "LICENSE" => return FileType::License,
             _ => {}
         }
     }
 
+    // for files with normal extensions
     match path.extension() {
         Some(ext) => match ext.to_string_lossy().as_ref() {
             "rs" => FileType::Rust,
@@ -58,27 +60,29 @@ pub fn get_file_type(file: String) -> FileType {
 }
 
 pub fn get_file_type_icon(file_type: FileType) -> String {
-    match file_type {
-        FileType::Rust => format!("{}", "".truecolor(206, 66, 43)),
-        FileType::Config => format!("{}", "".truecolor(128, 128, 128)),
-        FileType::C => format!("{}", "".truecolor(57, 74, 171)),
-        FileType::CPP => format!("{}", "".truecolor(0, 89, 156)),
-        FileType::CS => format!("{}", "󰌛".truecolor(149, 60, 173)),
-        FileType::Zig => format!("{}", "".truecolor(247, 164, 29)),
-        FileType::Python => format!("{}", "".truecolor(255, 224, 82)),
-        FileType::JavaScript => format!("{}", "".truecolor(240, 219, 79)),
-        FileType::TypeScript => format!("{}", "".truecolor(0, 122, 204)),
-        FileType::Html => format!("{}", "".truecolor(225, 78, 29)),
-        FileType::Css => format!("{}", "".truecolor(2, 119, 189)),
-        FileType::Scss => format!("{}", "".truecolor(205, 103, 153)),
-        FileType::React => format!("{}", "".truecolor(0, 216, 255)),
-        FileType::Git => format!("{}", "".truecolor(241, 80, 47)),
-        FileType::Lock => format!("{}", "".truecolor(244, 244, 244)),
-        FileType::Toml => format!("{}", "".truecolor(156, 66, 33)),
-        FileType::License => format!("{}", "".truecolor(249, 252, 33)),
-        FileType::Markdown => format!("{}", "".truecolor(244, 244, 244)),
-        _ => "".to_string(),
-    }
+    let file_type_icon = match file_type {
+        FileType::Rust => "".truecolor(206, 66, 43),
+        FileType::Config => "".truecolor(128, 128, 128),
+        FileType::C => "".truecolor(57, 74, 171),
+        FileType::CPP => "".truecolor(0, 89, 156),
+        FileType::CS => "󰌛".truecolor(149, 60, 173),
+        FileType::Zig => "".truecolor(247, 164, 29),
+        FileType::Python => "".truecolor(255, 224, 82),
+        FileType::JavaScript => "".truecolor(240, 219, 79),
+        FileType::TypeScript => "".truecolor(0, 122, 204),
+        FileType::Html => "".truecolor(225, 78, 29),
+        FileType::Css => "".truecolor(2, 119, 189),
+        FileType::Scss => "".truecolor(205, 103, 153),
+        FileType::React => "".truecolor(0, 216, 255),
+        FileType::Git => "".truecolor(241, 80, 47),
+        FileType::Lock => "".truecolor(244, 244, 244),
+        FileType::Toml => "".truecolor(156, 66, 33),
+        FileType::License => "".truecolor(249, 252, 33),
+        FileType::Markdown => "".truecolor(244, 244, 244),
+        _ => "".into(),
+    };
+
+    file_type_icon.to_string()
 }
 
 pub fn render_file(file_name: String, file_type: FileType) -> String {
